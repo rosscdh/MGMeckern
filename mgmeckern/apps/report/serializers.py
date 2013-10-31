de2b5fd9c703ece5b793b2e56b77cafd04d21710
Ross Crawford <sendrossemail@gmail.com>
2013-10-31 21:48:43 +0100

    worknig dynamic labels

--- mgmeckern/apps/report/serializers.py
+++ mgmeckern/apps/report/serializers.py
@@ -5,6 +5,9 @@ from .models import Report
 
 
 class ReportSerializer(serializers.ModelSerializer):
+    css_severity = serializers.CharField(source='css_severity', read_only=True, required=False)
+    display_severity = serializers.CharField(source='display_severity', read_only=True, required=False)
+
     class Meta:
         model = Report
         exclude = ('email',)
