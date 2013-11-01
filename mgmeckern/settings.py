6a4ca1be99f90bcdcf65d5eafd03819e5261ed0f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-01 12:28:41 +0100

    fixing static

--- mgmeckern/settings.py
+++ mgmeckern/settings.py
@@ -71,7 +71,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'static')
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -79,9 +79,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'static')
 )
 
 # List of finder classes that know how to find static files in
