ac41d2d5ae8591c9c0ec45967064d482d30563e0
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-01 10:18:21 +0100

    made forms more complete

--- mgmeckern/apps/report/forms.py
+++ mgmeckern/apps/report/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from parsley.decorators import parsleyfy
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.layout import Layout, Fieldset, Div, Submit, HTML, ButtonHolder, Button, Row, Field
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
 from . import SEVERITY_CHOICES
@@ -32,16 +32,20 @@ class ReportForm(forms.ModelForm):
 
 @parsleyfy
 class AddressSearchForm(forms.Form):
-    q = forms.CharField(label='', initial='', widget=forms.TextInput(attrs={'class': 'input-lg col-lg-12', 'placeholder': 'Street Address...'}))
+    q = forms.CharField(label='', initial='', required=True, widget=forms.TextInput(attrs={'class': 'input-lg col-lg-12', 'placeholder': 'Street Address...'}))
 
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
-        self.helper.form_class = 'form-inline'
-        #self.helper.form_id = 'address-search-form'
+        self.helper.form_class = 'form-inline parsley'
+        self.helper.field_template = 'bootstrap3/layout/inline_field.html'
+
         self.helper.layout = Layout(
-            Fieldset(
-                'Where is the problem?',
+            Field(
                 'q',
+            ),
+            ButtonHolder(
+                Button('btn-search-btn', 'Search', css_id='search-btn', css_class='btn btn-info'),
+                Button('btn-add-marker-btn', 'Add Report', css_id='add-marker-btn', css_class='btn btn-success'),
             ),
         )
         super(AddressSearchForm, self).__init__(*args, **kwargs)
