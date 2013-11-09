c5bce639869d0d1e3a7dac17c85eceffcd1ec4ca
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-09 11:06:28 +0100

    translated html files and made url translatable

--- mgmeckern/urls.py
+++ mgmeckern/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,5 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/', include('mgmeckern.apps.api.urls', namespace='api_v1')),
+)
+
+urlpatterns += i18n_patterns('',
     url(r'^', include('mgmeckern.apps.public.urls', namespace='public')),
 )
