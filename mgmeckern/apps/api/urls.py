1a4c6c84e976b3c8e83dd07f1b90df57462d6857
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-09 11:05:49 +0100

    made public urls translatable

--- mgmeckern/apps/api/urls.py
+++ mgmeckern/apps/api/urls.py
@@ -2,12 +2,14 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
-from mgmeckern.apps.report.views import ReportViewSet
+from mgmeckern.apps.report.views import ReportViewSet, DeletedReportViewSet
 
 
 router = routers.DefaultRouter()
 
+router.register(r'report/deleted', DeletedReportViewSet)
 router.register(r'report', ReportViewSet)
+
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
