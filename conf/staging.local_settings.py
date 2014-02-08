b071f56dbc587aeaa8d95378fd616f67e4b3170b
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-08 18:51:00 +0100

    added wsgi and local settings

---
+++ conf/staging.local_settings.py
@@ -0,0 +1,12 @@
+PROJECT_ENVIRONMENT = 'staging'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+MEDIA_URL = '/m/'
+STATIC_URL = '/s/'
+
+MEDIA_ROOT = '/home/rosscdh/webapps/htdocs/mgm/media/'
+STATICFILES_DIRS = (
+    '/home/rosscdh/webapps/htdocs/mgm/static/'
+)
---
+++ conf/staging.wsgi.py
@@ -0,0 +1,18 @@
+import os, sys, site
+
+site.addsitedir('/home/rosscdh/.virtualenvs/mgm/lib/python2.7/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mgmeckern.settings'
+
+activate_this = os.path.expanduser("~/.virtualenvs/mgm/bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
+
+# Calculate the path based on the location of the WSGI script
+project = '/home/rosscdh/webapps/mgm/mgm/'
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+
+sys.path = ['/home/rosscdh/webapps/mgm/mgm', '/home/rosscdh/webapps/mgm/mgm/apps', '/home/rosscdh/webapps/mgm'] + sys.path
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
