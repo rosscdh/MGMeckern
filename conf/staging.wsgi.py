ea7a4a94067b17edde0f5320396224d0f0ca04d2
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-17 09:30:59 +0100

    wsgi path

--- conf/staging.wsgi.py
+++ conf/staging.wsgi.py
@@ -8,11 +8,11 @@ activate_this = os.path.expanduser("~/.virtualenvs/mgm/bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
 
 # Calculate the path based on the location of the WSGI script
-project = '/home/rosscdh/webapps/mgm/mgm/'
+project = '/home/rosscdh/webapps/mgm/mgmeckern/'
 workspace = os.path.dirname(project)
 sys.path.append(workspace)
 
-sys.path = ['/home/rosscdh/webapps/mgm/mgm', '/home/rosscdh/webapps/mgm/mgm/apps', '/home/rosscdh/webapps/mgm'] + sys.path
+sys.path = ['/home/rosscdh/webapps/mgm/mgmeckern', '/home/rosscdh/webapps/mgm/mgmeckern/apps', '/home/rosscdh/webapps/mgm'] + sys.path
 
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
